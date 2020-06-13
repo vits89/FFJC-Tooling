9ea4ab2dc7c52eb5ad4d11fa125a61d8d54fde67
Vitalij Sorokin <sv89@tut.by>
2020-06-13 18:05:45 +0300

    Add plugins to webpack configuration

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: 'webpack example' })
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
